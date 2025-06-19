2651701b94dffbfcdffe46912b79741ed2ff73de
ChamashaFernando <chamashanethmi81@gmail.com>
2025-06-19 10:21:07 +0530

    Initial commit - add spring boot project

---
+++ src/test/java/lk/chamasha/jwt/authentication/JwtAuthenticationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.chamasha.jwt.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthenticationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
